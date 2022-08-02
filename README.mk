f0bdc8da8590f4b485aa0f725246edc8502469cc
Emil Velikov <emil.velikov@collabora.com>
2022-08-02 12:06:27 +0100

    README: Add profile to indicate org/project status
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

---
+++ README.mk
@@ -0,0 +1,12 @@
+# Archived, moved to https://gitlab.freedesktop.org/mesa/waffle/
+
+This GitHub organisation contains archived repositories, which have
+moved to https://gitlab.freedesktop.org/mesa/waffle/.
+
+The repositories are preserved, since the original development took
+place in GitHub.
+
+## Contributions
+
+Contributions are welcome, but please submit them to the upstream
+repository.
